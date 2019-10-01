916bd4021c0fe9dcdabd3289f45734f77e472143
sainsh <kfsainsh@gmail.com>
2019-10-01 17:28:55 +0200

    working on basic objects

---
+++ 4-objects/objects.js
@@ -0,0 +1,28 @@
+let a = { firstName: "Josh", lastname: "Lehman" };
+let b = { a: 1, b: 2, c: 3, d: 4 };
+let c = {
+    animal: "dog",
+    noise: "bark",
+    age: 3,
+    type: "Labrador",
+    color: "Yellow",
+}
+
+
+console.log("_________________________________");
+
+let Thomas = {
+    name : { 
+        first : "Thomas",
+        last : "Nielsen",
+        middle : "Andreas"
+    },
+    age : 26,
+    hometown : "Nakskov",
+    pets : ["Mirri", "Gavstriks"],
+    hobbies : ["pen and paper", "LARP", "gaming"],
+    occupation : "student"
+};
+
+console.log(Thomas);
+console.log(Thomas.name.first);
